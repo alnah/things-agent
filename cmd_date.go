671006c64bfedea8d460deeffe25d646553c67b3
alnah <alexis.nahan@gmail.com>
2026-03-09 19:49:20 -0300

    refactor: move date and version commands

--- cmd_date.go
+++ cmd_date.go
@@ -1,22 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
+	commandlib "github.com/alnah/things-agent/internal/command"
 	"github.com/spf13/cobra"
 )
 
 func newDateCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:   "date",
-		Short: "Show current weekday, date, time, and timezone",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Fprintln(cmd.OutOrStdout(), formatCurrentDate(time.Now()))
-		},
-	}
+	return commandlib.NewDateCmd(time.Now)
 }
 
 func formatCurrentDate(now time.Time) string {
-	return now.Format("Monday 2006-01-02 15:04:05 MST")
+	return commandlib.FormatCurrentDate(now)
 }
--- cmd_version.go
+++ cmd_version.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"fmt"
-
+	commandlib "github.com/alnah/things-agent/internal/command"
 	"github.com/spf13/cobra"
 )
 
 func newVersionCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:   "version",
-		Short: "Show CLI version",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Fprintln(cmd.OutOrStdout(), "things", effectiveCLIVersion())
-		},
-	}
+	return commandlib.NewVersionCmd(effectiveCLIVersion)
 }
